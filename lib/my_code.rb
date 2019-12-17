988162c6adf47d6952898159fb736c2cf7530b60
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-17 18:06:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,5 +49,13 @@ def reduce_to_total(source_array, starting_point)
   end
   return total
 end
-reduce_to_all_true(source_array)
-reduce_to_any_true(source_array)
+
+def reduce_to_total(source_array)
+  i = starting_point
+  total = 0
+  while i<source_array.length do
+    total += source_array[i]
+    i+=1
+  end
+  return total
+end
