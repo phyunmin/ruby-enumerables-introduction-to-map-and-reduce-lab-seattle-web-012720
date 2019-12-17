9d9a63e3a5553f977c5af1a81c74c7eda08b4930
Hyunmin Park <hyunmin.park.career@gmail.com>
2019-12-17 18:04:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,11 +8,11 @@ def map_to_negativize(source_array)
   end
 end
 
-def map_to_negativize(source_array)
+def map_to_no_change(source_array)
   i = 0
   array = []
   while i<source_array.length do
-    array[i] = source_array[i]*-1
+    array[i] = source_array[i]
     i+=1
   end
 end
